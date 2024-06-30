2f16a9585ca80392d3d64778262ca2a5042923fd
Callada-AnnaMarie-BSIT32E2 <161041733+Resurreccion-SamuelAngelo-BSIT32E2@users.noreply.github.com>
2024-06-30 22:12:41 +0800

    complete

---
+++ PREFINALS_AUTH_RESURRECCION_SAMUELANGELO_BSIT_32E2/ProtectedApi/Controllers/ValuesController.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+
+namespace ProtectedApi.Controllers
+{
+    [Authorize]
+    [ApiController]
+    [Route("api/[controller]")]
+    public class ValuesController : ControllerBase
+    {
+        [HttpGet]
+        public ActionResult<object> Get()
+        {
+            var user = HttpContext.User.Identity.Name;
+            var userSection = "32E2";
+            var userCourse = "Bachelor of Science in Information Technology";
+
+            var funFacts = new List<string>
+            {
+               
+                "My name is Samuel Angelo Resurreccion",
+                "I really love to play FPS games.",
+                "My Favorite hobby is collecting exotic animals and playing instruments."
+              
+                
+            };
+
+            // Random generator
+            var random = new Random();
+            var selectedFacts = new List<string>();
+            for (int i = 0; i < 3; i++)
+            {
+                var index = random.Next(funFacts.Count);
+                selectedFacts.Add(funFacts[index]);
+                funFacts.RemoveAt(index);
+            }
+
+            return new
+            {
+                UserName = user,
+                Section = userSection,
+                Course = userCourse,
+                FunFacts = selectedFacts
+            };
+        }
+    }
+}
---
+++ PREFINALS_AUTH_RESURRECCION_SAMUELANGELO_BSIT_32E2/ProtectedApi/Startup.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
+
+namespace ProtectedApi
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            // Configure JWT authentication
+            var key = Encoding.ASCII.GetBytes("sadgebols");
+
+            services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+                .AddJwtBearer(options =>
+                {
+                    options.TokenValidationParameters = new TokenValidationParameters
+                    {
+                        ValidateIssuer = true,
+                        ValidateAudience = true,
+                        ValidateLifetime = true,
+                        ValidateIssuerSigningKey = true,
+                        ValidIssuer = "popipo",
+                        ValidAudience = "catto",
+                        IssuerSigningKey = new SymmetricSecurityKey(key),
+                    };
+                });
+
+            services.AddControllers();
+        }
+
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            app.UseAuthentication();
+            app.UseAuthorization();
+
+            app.UseRouting();
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+        }
+    }
+}
